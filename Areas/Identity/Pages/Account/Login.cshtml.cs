55fb5c7d286f2380ff32c94aa6bdc35e31a2ac0a
Anna Kinzel <anna@kinzels.com>
2024-12-03 23:14:06 -0600

    redirecting where the pages goes after loggin in

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -91,7 +91,10 @@ namespace greenhouse.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/");
+            //
+            // THIS IS WHERE ITS BEING DIRECTED TO GO AFTER LOGGIN IN
+            //
+            returnUrl ??= Url.Content("~/blossom");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
@@ -103,7 +106,7 @@ namespace greenhouse.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/blossom");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
