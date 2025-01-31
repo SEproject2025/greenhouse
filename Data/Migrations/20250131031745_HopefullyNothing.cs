5dcda3a0a416c365ca029c9c6aeaf1ba303ad3b2
J-D-White <jared.white@students.pcci.edu>
2025-01-30 21:19:35 -0600

    Altered the fileds that the migrations forced us to alter and dealth with FK error: ASPnet tables exist and are linked to each other; plant table exists as it was before refactorization

---
+++ Data/Migrations/20250131031745_HopefullyNothing.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace greenhouse.Migrations
+{
+    /// <inheritdoc />
+    public partial class HopefullyNothing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
