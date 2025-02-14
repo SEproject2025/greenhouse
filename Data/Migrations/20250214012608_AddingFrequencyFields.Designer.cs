1eb37802dec5504c2bf8d9a0df3b728ec585c475
Anna Kinzel <anna@kinzels.com>
2025-02-13 19:33:39 -0600

    Added a migration for frequencies

--- Data/Migrations/20250214012608_AddingFrequencyFields.Designer.cs
+++ Data/Migrations/20250214012608_AddingFrequencyFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using greenhouse.Data;
 
@@ -11,9 +12,11 @@ using greenhouse.Data;
 namespace greenhouse.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250214012608_AddingFrequencyFields")]
+    partial class AddingFrequencyFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
