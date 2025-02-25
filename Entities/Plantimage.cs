c382ff5ad670bfaf6e1ed37fa7b4ac75039a26de
Logan Fortenberry <181684793+Low-gains@users.noreply.github.com>
2025-02-24 20:42:26 -0600

    Current testing state of code changes

--- Entities/Plantimage.cs
+++ Entities/Plantimage.cs
@@ -4,5 +4,8 @@
     {
         public string Name { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
+
+        // Add this property to store image data as a byte array
+        public byte[]? IMAGE_DATA { get; set; }
     }
 }
