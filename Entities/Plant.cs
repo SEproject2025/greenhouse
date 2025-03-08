f3d9c3905a6c40ea3800ddc2453ea54c10f34b12
danielkhodge <danielkhodge01@gmail.com>
2025-03-07 21:24:36 -0600

    Most changes complete. Blocked trying to make the other plants selectable.

--- Entities/Plant.cs
+++ Entities/Plant.cs
@@ -2,10 +2,10 @@
 {
     public class Plant
     {
-        public string Name { get; set; } = string.Empty;
-        public string ScientificName { get; set; } = string.Empty;
-        public string Image { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string Name                           { get; set; } = string.Empty;
+        public string ScientificName                 { get; set; } = string.Empty;
+        public string Image                          { get; set; } = string.Empty;
+        public string Description                    { get; set; } = string.Empty;
         public Dictionary<string, string> Attributes { get; set; } = new();
     }
 }
