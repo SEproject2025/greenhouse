e272a6b703d672e6210ee6ec7c4a837365ebb194
danielkhodge <danielkhodge01@gmail.com>
2025-03-07 20:47:44 -0600

    Plant object created. Data filled

---
+++ Entities/Plant.cs
@@ -0,0 +1,11 @@
+﻿namespace greenhouse.Entities
+{
+    public class Plant
+    {
+        public string Name { get; set; } = string.Empty;
+        public string ScientificName { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Dictionary<string, string> Attributes { get; set; } = new();
+    }
+}
